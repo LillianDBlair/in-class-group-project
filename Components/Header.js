4ff5e695b1e2ddf8171088627f14ef497b444cb8
Lillian Blair <lilliand_2000@yahoo.com>
2020-06-11 19:05:59 -0500

    Started SPA Changes

---
+++ Components/Header.js
@@ -0,0 +1,4 @@
+export default () => `
+<header>
+    <h1>Savvy Coders April 2020 Cohort</h1>
+  </header>`;
---
+++ Components/Nav.js
@@ -0,0 +1,9 @@
+export default () => `
+<nav>
+    <i class="fas fa-bars"></i>
+    <ul class="hidden--mobile nav-links">
+      <li><a href="#">Home</a></li>
+      <li><a href="about.html">About Savvy Coders</a></li>
+    </ul>
+  </nav>
+`;
